75fb27cb2c1893c615bf58eb1b404210b776dea8
Daydreamer <=>
2017-04-15 01:10:15 -0400

    Revert "revert estimate_remaining_time"
    This reverts commit dc748c3025107984680a35610a72b83c61b25721.

--- monocle/db.py
+++ monocle/db.py
@@ -707,33 +707,26 @@ def get_widest_range(session, spawn_id):
         .scalar()
 
 
-def estimate_remaining_time(session, spawn_id, seen=None):
+def estimate_remaining_time(session, spawn_id, seen):
     first, last = get_first_last(session, spawn_id)
 
     if not first:
         return 90, 1800
 
-    if seen:
-        if seen > last:
-            last = seen
-        elif seen < first:
-            first = seen
+    if seen > last:
+        last = seen
+    elif seen < first:
+        first = seen
 
     if last - first > 1710:
-        possible = (first + 90, last + 90, first + 1800, last + 1800)
-        estimates = []
-        for possibility in possible:
-            estimates.append(utils.time_until_time(possibility, seen))
-        soonest = min(estimates)
-        latest = max(estimates)
-        return soonest, latest
+        estimates = [
+            time_until_time(x, seen)
+            for x in (first + 90, last + 90, first + 1800, last + 1800)]
+        return min(estimates), max(estimates)
 
     soonest = last + 90
     latest = first + 1800
-    soonest = utils.time_until_time(soonest, seen)
-    latest = utils.time_until_time(latest, seen)
-
-    return soonest, latest
+    return time_until_time(soonest, seen), time_until_time(latest, seen)
 
 
 def get_punch_card(session):
